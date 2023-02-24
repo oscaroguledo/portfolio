a23b59f59597f66c625ad276bec9115d72458ada
Oscar <oscarchiagoziem@gmail.com>
2023-02-24 18:32:00 +0100

    edited html and css file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+
+
+(function($) {
+
+  // Add smooth scrolling to all links in navbar
+  $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a, a.btn-warning").on('click', function(event) {
+
+    var hash = this.hash;
+    if (hash) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function() {
+        window.location.hash = hash;
+      });
+    }
+
+  });
+
+  $(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+
+  //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar-default").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+
+})(jQuery);
+
+//==========================typed.js implementation====================
+
 var data_items= document.getElementById('typed').dataset.typedItems;
 data_items=data_items.split(',');
 console.log(data_items);
